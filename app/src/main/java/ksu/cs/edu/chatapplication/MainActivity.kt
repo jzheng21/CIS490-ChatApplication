b0f827779f60759e91bb2a930b22749522958293
Jie <jayie21@live.com>
2018-06-24 15:00:36 -0500

    Minor Changes

--- app/src/main/java/ksu/cs/edu/chatapplication/MainActivity.kt
+++ app/src/main/java/ksu/cs/edu/chatapplication/MainActivity.kt
@@ -2,11 +2,14 @@ package ksu.cs.edu.chatapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var firebaseDatabase = FirebaseDatabase.getInstance()
     }
 }
