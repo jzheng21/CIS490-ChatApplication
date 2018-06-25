5aa206179576d9a17969ecc0dbedf9a3a7242306
Jie <jayie21@live.com>
2018-06-24 20:55:23 -0500

    Move threads selection from main

--- app/src/main/java/ksu/cs/edu/chatapplication/MainActivity.kt
+++ app/src/main/java/ksu/cs/edu/chatapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package ksu.cs.edu.chatapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import com.google.firebase.database.ChildEventListener
@@ -16,37 +18,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = FirebaseDatabase.getInstance()
-        val ref = db.getReference("threads")
 
-        ref.addChildEventListener(object : ChildEventListener{
-            override fun onCancelled(error: DatabaseError) {
-                Log.w("Firebase Database Error", error?.toException())
-            }
+    }
 
-            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
-                Log.w("Thread Selection Error", "Threads cannot be moved")
-            }
-
-            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onChildAdded(dataSnapshot : DataSnapshot, p1: String?) {
-                if(dataSnapshot is DataSnapshot){
-                    val discussionThread = dataSnapshot.getValue(DiscussionThread::class.java)
-                    if(discussionThread is DiscussionThread) {
-                        val linearLayout = findViewById<LinearLayout>(R.id.linearLayoutThreads)
-                        val launchThreadButton = Button(this@MainActivity)
-                        launchThreadButton.text = discussionThread.title
-                        linearLayout.addView(launchThreadButton)
-                    }
-                }
-            }
-
-            override fun onChildRemoved(p0: DataSnapshot) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        })
+    fun launchSelections(view : View){
+        val intent = Intent(this, SelectionActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/ksu/cs/edu/chatapplication/SelectionActivity.kt
@@ -0,0 +1,52 @@
+package ksu.cs.edu.chatapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.LinearLayout
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+
+class SelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selection)
+
+        val db = FirebaseDatabase.getInstance()
+        val ref = db.getReference("threads")
+
+        ref.addChildEventListener(object : ChildEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("Firebase Database Error", error?.toException())
+            }
+
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+                Log.w("Thread Selection Error", "Threads cannot be moved")
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildAdded(dataSnapshot : DataSnapshot, p1: String?) {
+                if(dataSnapshot is DataSnapshot){
+                    val discussionThread = dataSnapshot.getValue(DiscussionThread::class.java)
+                    if(discussionThread is DiscussionThread) {
+                        val linearLayout = findViewById<LinearLayout>(R.id.linearLayoutThreads)
+                        val launchThreadButton = Button(this@SelectionActivity)
+                        launchThreadButton.text = discussionThread.title
+                        linearLayout.addView(launchThreadButton)
+                    }
+                }
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+    }
+}
